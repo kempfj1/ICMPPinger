41e358869c5efd2b52df11c51b0cc8d65e9a82a2
theRealMonning <rozzipeter@gmail.com>
2017-04-10 19:25:17 -0400

    combined the files

--- ICMPPinger.py
+++ ICMPPinger.py
@@ -122,47 +122,48 @@ def get_route(hostname):
     timeLeft = TIMEOUT
     for ttl in xrange(1, MAX_HOPS):
         for tries in xrange(TRIES):
-        destAddr = gethostbyname(hostname)
-        # Fill in start
-        # Make a raw socket named mySocket
-        # Fill in end
-        mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
-        mySocket.settimeout(TIMEOUT)
-        try:
-            d = build_packet()
-            mySocket.sendto(d, (hostname, 0))
-            t = time.time()
-            startedSelect = time.time()
-            whatReady = select.select([mySocket], [], [], timeLeft)
-            howLongInSelect = (time.time() - startedSelect)
-            if whatReady[0] == []:  # Timeout
-                print " * * * Request timed out."
-            recvPacket, addr = mySocket.recvfrom(1024)
-            timeReceived = time.time()
-            timeLeft = timeLeft - howLongInSelect
-            if timeLeft <= 0:
-                print " * * * Request timed out."
-        except timeout:
-            continue
-        else:
-            if type == 11:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - t) * 1000, addr[0])
-            elif type == 3:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - t) * 1000, addr[0])
-            elif type == 0:
-                bytes = struct.calcsize("d")
-                timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - timeSent) * 1000, addr[0])
-                return
+            destAddr = gethostbyname(hostname)
+            # Fill in start
+            icmp = getprotobyname("icmp")
+            mySocket = socket(AF_INET, SOCK_RAW, icmp)
+            # Fill in end
+            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
+            mySocket.settimeout(TIMEOUT)
+            try:
+                d = build_packet()
+                mySocket.sendto(d, (hostname, 0))
+                t = time.time()
+                startedSelect = time.time()
+                whatReady = select.select([mySocket], [], [], timeLeft)
+                howLongInSelect = (time.time() - startedSelect)
+                if whatReady[0] == []:  # Timeout
+                    print " * * * Request timed out."
+                recvPacket, addr = mySocket.recvfrom(1024)
+                timeReceived = time.time()
+                timeLeft = timeLeft - howLongInSelect
+                if timeLeft <= 0:
+                    print " * * * Request timed out."
+            except timeout:
+                continue
             else:
-                print "error"
-            break
-        finally:
-            mySocket.close()
+                if type == 11:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - t) * 1000, addr[0])
+                elif type == 3:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - t) * 1000, addr[0])
+                elif type == 0:
+                    bytes = struct.calcsize("d")
+                    timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
+                    print " %d rtt=%.0f ms %s" % (ttl,(timeReceived - timeSent) * 1000, addr[0])
+                    return
+                else:
+                    print "error"
+                break
+            finally:
+                mySocket.close()
 
 def ping(host, timeout=1):
     #timeout=1 means: If one second goes by without a reply from the server, ping or pong is lost
